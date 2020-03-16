be10f7192b8e746f873eee08f6a4dfba9c8ce948
Ana Fretes <anafretes@fpuna.edu.py>
2020-03-16 00:05:38 -0300

    Levantando nueva versión de autor Jorge Gauto por problemas de conexión

--- uptask3/app/Http/Controllers/Controller.php
+++ uptask3/app/Http/Controllers/Controller.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Database\Eloquent\Model;//agregado para buscar la solucion de editar
 
 class Controller extends BaseController
 {
