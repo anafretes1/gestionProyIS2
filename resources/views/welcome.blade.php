93718d8683fa0523616ce0274389b9e991bd057f
jorgegauto <jorgegauto19@gmail.com>
2020-03-22 15:18:06 -0400

    Modificada la zona horaria y el lenguaje del sistema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>UpTask 4.0</title>
+        <title>UpTask 5.0</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -79,7 +79,7 @@
             @endif
             <div class="content">
                 <div class="title m-b-md">
-                    UpTask 4.0
+                    UpTask 5.0
                 </div>
             </div>
         </div>
