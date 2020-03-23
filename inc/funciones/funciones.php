978cc8a494e7aefdada812f43f41c48c09883182
Juan Recalde <jrecalde@gmail.com>
2020-03-22 20:22:19 -0400

    Housekeeping 2
    para liberar DEVELOP

--- inc/funciones/funciones.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Obtiene la pagina atual que se ejecuta
-function obtenerPaginaActual(){
-    $archivo = basename($_SERVER['PHP_SELF']);
-    $pagina = str_replace(".php","",$archivo);
-    return $pagina;
-}
--- inc/templates/barra.php
+++
@@ -1,4 +0,0 @@
-<div class="barra">
-    <h1>UpTask - Administración de Proyectos</h1>
-    <a href="login.php?cerrar_sesion=true">Cerrar Sesión</a><!--se enlaza al login por medio de un parametro seteado a true -->
-</div>
